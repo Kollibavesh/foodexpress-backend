477db536ee96d0cf6a88fd89d9067b5826e75434
Bavesh Kolli <baveshkolli@Mac-891.lan>
2026-03-09 22:55:40 -0400

    disable spring security

--- src/main/java/com/bavesh/foodapp/FoodappApplication.java
+++ src/main/java/com/bavesh/foodapp/FoodappApplication.java
@@ -2,14 +2,13 @@ package com.bavesh.foodapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(
-		exclude = {UserDetailsServiceAutoConfiguration.class}
-)
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class FoodappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FoodappApplication.class, args);
 	}
+
 }
